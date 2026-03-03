c0a80806c767cd3704117479edd03a51a60127a2
Ambasibryton <ambasibryton@gmail.com>
2026-03-03 23:22:03 +0300

    Add TODOs for product management and layout components
    Added TODOs for components and functionality in App.jsx.

--- App.jsx
+++ App.jsx
@@ -1,17 +1,34 @@
 // React Challenge Task
 
-// TODO 1: Create state for products
-// TODO 2: Create state for cart
-// TODO 3: Fetch products from:
-// https://fakestoreapi.com/products
-// TODO 4: Create addToCart function
-// TODO 5: Display products using map()
-// TODO 6: Show cart count
+import { useState, useEffect } from "react";
+
+// TODO 1: Create Navbar component
+// TODO 2: Create Sidebar component
+// TODO 3: Create ProductCard component
+// TODO 4: Create Footer component
 
 export default function App() {
+  // TODO 5: Create state for products
+  // TODO 6: Create state for cart
+
+  // TODO 7: Fetch products using useEffect
+  // Must display at least 8 items
+
+  // TODO 8: Create addToCart function
+
   return (
-    <div>
-      <h1>E-Commerce React Challenge</h1>
+    <div className="app">
+      {/* TODO 9: Add Navbar */}
+
+      <div className="layout">
+        {/* TODO 10: Add Sidebar */}
+
+        <main className="products">
+          {/* TODO 11: Map products here */}
+        </main>
+      </div>
+
+      {/* TODO 12: Add Footer */}
     </div>
   );
 }
