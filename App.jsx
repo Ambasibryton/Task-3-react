80ee7e840027b8ca493c894c723a15c1d1955b09
Ambasibryton <ambasibryton@gmail.com>
2026-03-03 22:41:06 +0300

    Initialize App component with TODOs for functionality
    Initial setup for E-Commerce React Challenge with TODOs for state management and product fetching.

---
+++ App.jsx
@@ -0,0 +1,17 @@
+// React Challenge Task
+
+// TODO 1: Create state for products
+// TODO 2: Create state for cart
+// TODO 3: Fetch products from:
+// https://fakestoreapi.com/products
+// TODO 4: Create addToCart function
+// TODO 5: Display products using map()
+// TODO 6: Show cart count
+
+export default function App() {
+  return (
+    <div>
+      <h1>E-Commerce React Challenge</h1>
+    </div>
+  );
+}
